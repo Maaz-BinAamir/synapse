92f3850cdf40797b1105a01f8d91ad2baf559680
AyeshaImran066 <ayeshaimran0662@gmail.com>
2025-11-20 21:05:52 +0500

    Styled authentication pages

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,10 +1,16 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import Image from "next/image";
 
 const signInSchema = z.object({
   email: z.email("Enter a valid email"),
@@ -43,28 +49,83 @@ export default function SignInPage() {
   });
 
   return (
-    <form onSubmit={onSubmit} className="space-y-4">
-      <label className="flex flex-col gap-1">
-        <span>Email</span>
-        <input type="email" {...register("email")} disabled={isPending} />
-        {errors.email && (
-          <p className="text-sm text-red-500">{errors.email.message}</p>
-        )}
-      </label>
+    <div className="flex h-screen">
+      <div className="w-1/3 flex items-center justify-center">
+        <div className="relative w-3/4 h-3/4">
+          <Image
+            src="/signin.png"
+            alt="Signin illustration"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </div>
 
-      <label className="flex flex-col gap-1">
-        <span>Password</span>
-        <input type="password" {...register("password")} disabled={isPending} />
-        {errors.password && (
-          <p className="text-sm text-red-500">{errors.password.message}</p>
-        )}
-      </label>
+      <div className="w-2/3 flex items-center justify-center">
+        <Card className="bg-[#9D83C4] p-8 rounded-lg w-full h-full flex flex-col justify-center">
+          <CardHeader>
+            <CardTitle className="text-white text-4xl text-center">
+              Log In
+            </CardTitle>
+          </CardHeader>
+          <form onSubmit={onSubmit} className="space-y-4">
+            <Label className="flex flex-col gap-1">
+              <span className="text-white">Email</span>
+              <Input
+                className="w-1/2"
+                type="email"
+                {...register("email")}
+                disabled={isPending}
+              />
+              {errors.email && (
+                <p className="text-sm text-red-500">{errors.email.message}</p>
+              )}
+            </Label>
 
-      {serverError && <p className="text-sm text-red-500">{serverError}</p>}
+            <Label className="flex flex-col gap-1">
+              <span className="text-white">Password</span>
+              <Input
+                className="w-1/2"
+                type="password"
+                {...register("password")}
+                disabled={isPending}
+              />
+              {errors.password && (
+                <p className="text-sm text-red-500">
+                  {errors.password.message}
+                </p>
+              )}
+            </Label>
 
-      <button type="submit" disabled={isPending}>
-        {isPending ? "Signing in…" : "Sign in"}
-      </button>
-    </form>
+            {serverError && (
+              <p className="text-sm text-red-500">{serverError}</p>
+            )}
+
+            <div className="flex justify-center">
+              <Button
+                className="bg-[#9D83C4] hover:bg-[#7a64a8] border border-white px-10 py-5 text-2xl"
+                type="submit"
+                disabled={isPending}
+              >
+                {isPending ? "Signing in…" : "Sign in"}
+              </Button>
+            </div>
+
+            <p className="text-white flex justify-center">
+              Do not have an account?{" "}
+              <Link className=" underline ml-2" href="/signup">
+                Register now
+              </Link>
+            </p>
+
+            <div className="flex justify-center">
+              <Button className="bg-[#9D83C4] hover:bg-[#7a64a8] border border-white px-10 py-5 text-2xl">
+                Login with Google
+              </Button>
+            </div>
+          </form>
+        </Card>
+      </div>
+    </div>
   );
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,16 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import Image from "next/image";
 
 const signUpSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters"),
@@ -44,36 +50,96 @@ export default function SignUpPage() {
   });
 
   return (
-    <form onSubmit={onSubmit} className="space-y-4">
-      <label className="flex flex-col gap-1">
-        <span>Name</span>
-        <input type="text" {...register("name")} disabled={isPending} />
-        {errors.name && (
-          <p className="text-sm text-red-500">{errors.name.message}</p>
-        )}
-      </label>
+    <div className="flex h-screen">
+      <div className="w-1/3 flex items-center justify-center">
+        <div className="relative w-3/4 h-3/4">
+          <Image
+            src="/signup.png"
+            alt="Signup illustration"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </div>
 
-      <label className="flex flex-col gap-1">
-        <span>Email</span>
-        <input type="email" {...register("email")} disabled={isPending} />
-        {errors.email && (
-          <p className="text-sm text-red-500">{errors.email.message}</p>
-        )}
-      </label>
+      <div className="w-2/3 grid place-items-center">
+        <Card className="bg-[#9D83C4] p-8 rounded-lg w-full h-full flex flex-col justify-center">
+          <CardHeader>
+            <CardTitle className="text-white text-4xl text-center">
+              Sign Up
+            </CardTitle>
+          </CardHeader>
+          <form onSubmit={onSubmit} className="space-y-4">
+            <Label className="flex flex-col gap-1">
+              <span className="text-white">Name</span>
+              <Input
+                className="w-1/2"
+                type="text"
+                {...register("name")}
+                disabled={isPending}
+              />
+              {errors.name && (
+                <p className="text-sm text-red-500">{errors.name.message}</p>
+              )}
+            </Label>
 
-      <label className="flex flex-col gap-1">
-        <span>Password</span>
-        <input type="password" {...register("password")} disabled={isPending} />
-        {errors.password && (
-          <p className="text-sm text-red-500">{errors.password.message}</p>
-        )}
-      </label>
+            <Label className="flex flex-col gap-1">
+              <span className="text-white">Email</span>
+              <Input
+                className="w-1/2"
+                type="email"
+                {...register("email")}
+                disabled={isPending}
+              />
+              {errors.email && (
+                <p className="text-sm text-red-500">{errors.email.message}</p>
+              )}
+            </Label>
 
-      {serverError && <p className="text-sm text-red-500">{serverError}</p>}
+            <Label className="flex flex-col gap-1">
+              <span className="text-white">Password</span>
+              <Input
+                className="w-1/2"
+                type="password"
+                {...register("password")}
+                disabled={isPending}
+              />
+              {errors.password && (
+                <p className="text-sm text-red-500">
+                  {errors.password.message}
+                </p>
+              )}
+            </Label>
 
-      <button type="submit" disabled={isPending}>
-        {isPending ? "Creating account…" : "Sign up"}
-      </button>
-    </form>
+            {serverError && (
+              <p className="text-sm text-red-500">{serverError}</p>
+            )}
+
+            <div className="flex justify-center">
+              <Button
+                className="bg-[#9D83C4] hover:bg-[#7a64a8] border border-white px-10 py-5 text-2xl"
+                type="submit"
+                disabled={isPending}
+              >
+                {isPending ? "Creating account…" : "Sign up"}
+              </Button>
+            </div>
+
+            <p className="text-white flex justify-center">
+              Already have an account?{" "}
+              <Link className=" underline ml-2" href="/signin">
+                Signin
+              </Link>
+            </p>
+
+            <div className="flex justify-center">
+              <Button className="bg-[#9D83C4] hover:bg-[#7a64a8] border border-white px-10 py-5 text-2xl">
+                Signup with Google
+              </Button>
+            </div>
+          </form>
+        </Card>
+      </div>
+    </div>
   );
 }
